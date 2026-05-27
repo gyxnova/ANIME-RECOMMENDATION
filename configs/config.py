954eed798099737ded4ae61b8d66303868a67df8
Gayatri <gsharma27084@gmail.com>
2026-05-27 12:10:01 +0530

    updated

--- configs/config.py
+++ configs/config.py
@@ -6,3 +6,4 @@ ROOT     = Path(__file__).resolve().parent.parent
 DATA_DIR = ROOT / "data" / "raw"
 DATA_EXT = ROOT / "data" / "external"
 DATA_PROC = ROOT / "data" / "processed"
+DATA_FEATURES = ROOT / "data" / "features"
